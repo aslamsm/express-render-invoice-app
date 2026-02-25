772732c8c0747e643dd5d7be2a039033fde36241
aslamsm <aslamsm@yahoo.com>
2026-02-25 08:36:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // local host port : 5173... render port : 3000... server port : 5000
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: ["http://localhost:3000", "http://localhost:5000"],
   }),
 );
 
