a04e526ab6cd14a1778bc39038260647afd1a38f
aslamsm <aslamsm@yahoo.com>
2026-02-25 10:02:33 +0300

    url update

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(
 // =======================
 
 mongoose
-  .connect(process.env.MONGO_URL || 5000)
+  .connect(process.env.MONGO_URL || "")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
