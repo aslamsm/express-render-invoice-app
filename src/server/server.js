bf09713c6ba4a6b245219033a6884295e1c84893
aslamsm <aslamsm@yahoo.com>
2026-02-25 11:37:22 +0300

    update srv

---
+++ src/server/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import customerRoutes from "./routes/customers.js";
+import itemRoutes from "./routes/Items.js";
+import invoiceRoutes from "./routes/invoices.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+// local host port : 5173... render port : 3000... server port : 5000
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+  }),
+);
+
+// =======================
+// MongoDB Connection
+// =======================
+
+mongoose
+  .connect(process.env.MONGO_URL || "")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Routes
+app.use("/customers", customerRoutes);
+app.use("/items", itemRoutes);
+app.use("/invoices", invoiceRoutes);
+
+// Start Server
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`),
+);
